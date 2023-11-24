5d0c5856b8d5e72a0168eee907204453b51c92da
dusengepeggy <135857264+dusengepeggy@users.noreply.github.com>
2023-11-24 10:46:18 +0200

    currency converter

---
+++ currencyConverter.js
@@ -0,0 +1,22 @@
+//money converter from rwf to dollars and vice versa
+const convert=(amount,currency)=>{
+    var newAmount;
+    const val=1237.59;
+   switch (currency) {
+    case '$':
+        newAmount=amount / val;
+        break;
+    case "frw": 
+        newAmount=amount * val;
+        break;
+    default:
+        newAmount="Unable to convert to that currency";
+        break;
+    }
+   return newAmount;
+
+
+}
+
+console.log(convert(100 ,'frw'));
+console.log(convert(100000 ,'frw'));
